93f2fd99cabbdeb49d1f168c66dbe9b9972d908d
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-03-03 10:04:19 -0300

    update: rename idx_exemplo to idx_fts_fatoitemnfe

--- sql/03_query_search_function.sql
+++ sql/03_query_search_function.sql
@@ -11,7 +11,7 @@ CREATE EXTENSION pg_trgm;
 
 
 --Cria índice na tabela fatoitemnfe
-CREATE INDEX idx_exemplo ON app.fatoitemnfe USING GIN (to_tsvector('portuguese', infnfe_det_prod_xprod));
+CREATE INDEX idx_fts_fatoitemnfe ON app.fatoitemnfe USING GIN (to_tsvector('portuguese', infnfe_det_prod_xprod));
 
 --CRIA TABELA PARA BUSCA
 CREATE TABLE app.search (
