8466a5ff62069933a234795458ec3fc0f4fa084c
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-06-08 15:47:34 -0300

    feat: latest_dfe_dhemi VIEW

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ COPY ./sql/05_refresh_materialized_views.sql /docker-entrypoint-initdb.d
 COPY ./sql/06_async_downloads_table.sql /docker-entrypoint-initdb.d
 COPY ./sql/07_async_reports_table.sql /docker-entrypoint-initdb.d
 COPY ./sql/08_async_reports_table.sql /docker-entrypoint-initdb.d
+COPY ./sql/09_dhemi_view.sql /docker-entrypoint-initdb.d
 COPY ./sql/99_update_permissions.sql /docker-entrypoint-initdb.d
 COPY ./data/app_efd-2020_09_17.sql /docker-entrypoint-initdb.d
 COPY ./data/app_fatonfe-2020_09_17.sql /docker-entrypoint-initdb.d
---
+++ sql/09_dhemi_view.sql
@@ -0,0 +1,20 @@
+-- change database and user
+\connect datalake datalakeuser
+
+
+-- Cria VIEW com as datas de emissão mais recentes para as tabelas: FatoNFe, FatoCTe, FatoMDFe, FatoEventoMDFe, FatoEventoCTe
+CREATE VIEW app.latest_dfe_dhemi_view AS 
+	SELECT *
+	FROM (
+			 (SELECT MAX("infnfe_ide_dhemi") as nfe FROM app.fatonfe) as dhnfe 
+			 NATURAL FULL OUTER JOIN 
+			 (SELECT MAX("infcte_ide_dhemi") as cte FROM app.fatocte) as dhcte
+			 NATURAL FULL OUTER JOIN
+			 (SELECT MAX("infmdfe_ide_dhemi") as mdfe FROM app.fatomdfe) as dhmdfe
+			 NATURAL FULL OUTER JOIN
+			 (SELECT MAX("eventomdfe_infevento_dhevento") as eventomdfe FROM app.fatoeventomdfe) as dheventomdfe
+			 NATURAL FULL OUTER JOIN
+			 (SELECT MAX("eventocte_infevento_dhevento") as eventocte FROM app.fatoeventocte) as dheventocte
+
+		 ) as temp;
+
