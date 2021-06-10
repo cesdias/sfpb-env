5fe3ef23cfbddcb50011313996fb85d25cb0a8da
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-06-10 15:52:32 -0300

    update: dhemi view

--- sql/09_dhemi_view.sql
+++ sql/09_dhemi_view.sql
@@ -6,15 +6,17 @@
 CREATE VIEW app.latest_dfe_dhemi_view AS 
 	SELECT *
 	FROM (
-			 (SELECT MAX("infnfe_ide_dhemi") as nfe FROM app.fatonfe) as dhnfe 
-			 NATURAL FULL OUTER JOIN 
-			 (SELECT MAX("infcte_ide_dhemi") as cte FROM app.fatocte) as dhcte
-			 NATURAL FULL OUTER JOIN
-			 (SELECT MAX("infmdfe_ide_dhemi") as mdfe FROM app.fatomdfe) as dhmdfe
-			 NATURAL FULL OUTER JOIN
-			 (SELECT MAX("eventomdfe_infevento_dhevento") as eventomdfe FROM app.fatoeventomdfe) as dheventomdfe
-			 NATURAL FULL OUTER JOIN
-			 (SELECT MAX("eventocte_infevento_dhevento") as eventocte FROM app.fatoeventocte) as dheventocte
+			(SELECT MAX("infnfe_ide_dhemi") as nfe FROM app.fatonfe where infnfe_ide_mod = 55) as dhnfe 
+			NATURAL FULL OUTER JOIN
+			(SELECT MAX("infnfe_ide_dhemi") as nfce FROM app.fatonfe where infnfe_ide_mod = 65) as dhnfce 
+			NATURAL FULL OUTER JOIN 
+			(SELECT MAX("infcte_ide_dhemi") as cte FROM app.fatocte) as dhcte
+			NATURAL FULL OUTER JOIN
+			(SELECT MAX("infmdfe_ide_dhemi") as mdfe FROM app.fatomdfe) as dhmdfe
+			NATURAL FULL OUTER JOIN
+			(SELECT MAX("eventomdfe_infevento_dhevento") as eventomdfe FROM app.fatoeventomdfe) as dheventomdfe
+			NATURAL FULL OUTER JOIN
+			(SELECT MAX("eventocte_infevento_dhevento") as eventocte FROM app.fatoeventocte) as dheventocte
 
-		 ) as temp;
+		) as temp;
 
